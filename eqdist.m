e5bea93e0ddd43347d9a24833728c119f608ec46
John <potatoking.john@gmail.com>
2015-11-27 22:30:26 -0700

    update

---
+++ eqdist.m
@@ -0,0 +1,17 @@
+function [D]=eqdist(i,x)
+
+% function: [D]=dist(i,x)
+%
+% Aim: 
+% Calculates the Euclidean distances between the i-th object and all objects in x	 
+%								    
+% Input: 
+% i - an object (1,n)
+% x - data matrix (m,n); m-objects, n-variables	    
+%                                                                 
+% Output: 
+% D - Euclidean distance (m,1)
+
+[m, n] = size(x);
+D = transpose(sum((((ones(m,1)*i)-x).^2),2));
+
